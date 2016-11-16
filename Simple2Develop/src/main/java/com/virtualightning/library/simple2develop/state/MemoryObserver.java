3125f0d15b6c0a33f60232c2bc77170ac33e1483
CimZzz <1152564696@qq.com>
2016-11-16 23:11:27 +0800

    Version 0.2.3 Provide some method to register observer,and add run type of observer

--- Simple2Develop/src/main/java/com/virtualightning/library/simple2develop/state/MemoryObserver.java
+++ Simple2Develop/src/main/java/com/virtualightning/library/simple2develop/state/MemoryObserver.java
@@ -4,6 +4,7 @@ package com.virtualightning.library.simple2develop.state;
  * Created by CimZzz on 16/7/22.<br>
  * Project Name : Virtual-Lightning Simple2Develop<br>
  * Since : VLSimple2Develop_0.0.1<br>
+ * Modify : VLSimple2Develop_0.2.3 将观察者运行环境作为可选项，其余方法标记为过时<br>
  * Description:<br>
  * 记忆状态观察者<br>
  * 只有当更新的状态与内部的旧状态不一致时才会执行更新
@@ -11,6 +12,11 @@ package com.virtualightning.library.simple2develop.state;
 @SuppressWarnings("unused")
 public abstract class MemoryObserver extends Observer{
     private Boolean oldState;
+
+    /**
+     * @since VLSimple2Develop_0.2.3
+     */
+    public MemoryObserver() {}
 
     public MemoryObserver(boolean runWhenStop) {
         super(runWhenStop);
